1d2218cb45ff1bcda360bc5db174f64f771952ff
DarStar <merlanor@gmail.com>
2022-12-01 19:29:37 +0300

    added task 3 and 5

--- PyHW3.py
+++ PyHW3.py
@@ -43,10 +43,16 @@
 
 # - [1.1, 1.2, 3.1, 10.01] => 0.19
 
-# something = [1.1, 1.2, 3.1, 10.01]
+# somethingborrowed = [1.1, 1.2, 3.1, 10.01]
+# somethingnew = []
+# i = 0
 
-# for i in something:
+# while i < len(somethingborrowed):
+#     dp = somethingborrowed[i] % 1
+#     somethingnew.append((round(dp, 2)))
+#     i += 1
 
+# print(max(somethingnew) - min(somethingnew))
 
 # 4. Напишите программу, которая будет преобразовывать десятичное число в двоичное.
 
@@ -62,17 +68,48 @@
 # def binar (x, y):
 #     while x > 0:
 #         a = str(x % 2)
-#         l.append(a)
+#         y.append(a)
 #         x = x // 2
-#     l.reverse()
-#     print(" ".join(l))
+#     y.reverse()
+#     print(" ".join(y))
 
 # binar(n, l)
-
-
 
 # 5. Задайте число. Составьте список чисел Фибоначчи, в том числе для отрицательных индексов.
 
 # Пример:
 
 # - для k = 8 список будет выглядеть так: [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21]
+
+# k = int(input("Enter number: "))
+# lneg = [1, -1]
+# lpos = [0, 1, 1]
+
+# def fib_pos(k, lpos):
+#     i = 0
+#     f1 = 1
+#     f2 = 1
+#     while i < k - 2:
+#         fsum = f1 + f2
+#         f1 = f2
+#         f2 = fsum
+#         lpos.append(f2)
+#         i += 1
+#     return lpos   
+ 
+# def fib_neg(k, lneg):
+#     f1 = 1
+#     f2 = -1
+#     i = 0
+#     while i < k - 2:
+#         fsum = f1 - f2
+#         f1 = f2
+#         f2 = fsum
+#         lneg.append(f2)
+#         i += 1
+#     lneg.reverse()
+#     return lneg   
+
+# fib_neg(k, lneg)
+# fib_pos(k, lpos)
+# print(lneg + lpos)
