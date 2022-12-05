fb7ff3787f05c2bf50fa8d3a1c0779208cf29bf3
DarStar <merlanor@gmail.com>
2022-12-05 22:16:57 +0300

    created sem and hw 4

---
+++ PySem4.py
@@ -0,0 +1,28 @@
+# from pathlib import Path
+
+# file_path = Path('c', r'folder', 'test.txt')
+# with open(file_path, 'r') as f_data:
+# print(f_data.read())
+
+# 1. Считать строку из набора чисел из файла. Напишите программу, которая покажет большее и меньшее число.
+# В качестве символа-разделителя используйте пробел. Результат заисать в конец исходного файла (х1 х2).
+
+# with open('file.txt', 'w') as data:
+#     data.write(f'3 56 8 45 7 10 23\n')
+
+# with open('file.txt', 'r') as data:
+#     lst = data.read()
+# lst2 = lst.split(' ')
+# lst3 =[int(i) for i in lst2]
+# maxn = max(lst3)
+# minx = min(lst3)
+
+# with open('file.txt', 'a') as data:
+#     data.write(f'max = {maxn} \n')
+#     data.write(f'min = {minx} \n')
+
+# 2. Найдите корни квадратного уравнения Ax² + Bx + C = 0 двумя способами (получить кортеж):
+
+# 1) с помощью математических формул нахождения корней квадратного уравнения
+# 2) с помощью дополнительных библиотек Python
+
